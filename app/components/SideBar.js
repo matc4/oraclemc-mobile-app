94278f311ad7a4d8ebd1b49ea59c436f7d047bd8
Matias Sebastian Cortes <matias.s.cortes@gmail.com>
2017-11-12 10:38:06 -0300

    fix drawer bar

--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -2,7 +2,7 @@
 
 import React, {Component, PropTypes} from "react";
 import { View  } from 'react-native';
-import { Drawer, Container, Content, Button, Text, Form, Item, Input } from 'native-base';
+import {Container, Content, Left, Right, Form, ListItem,List, Icon, Body, Text, Switch } from 'native-base';
 
 
 class SideBar extends Component {
@@ -13,11 +13,47 @@ class SideBar extends Component {
 
   render() {
     return (
-        <View style={{flex:1, backgroundColor:"white"}}>
-          <Text>
-            HOLA
-          </Text>
-        </View>
+      <View style={{flex:1, backgroundColor:"white"}}>
+      <Container>
+        <Content>
+          <List>
+            <ListItem icon>
+              <Left>
+                <Icon name="plane" />
+              </Left>
+              <Body>
+                <Text>Offline Mode</Text>
+              </Body>
+              <Right>
+                <Switch value={false} />
+              </Right>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="film" />
+              </Left>
+              <Body>
+                <Text>Peliculas</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="cart" />
+              </Left>
+              <Body>
+                <Text>Comprar</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+      </View>
     );
   }
 };
